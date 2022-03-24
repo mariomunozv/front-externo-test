3be7b5986506dead8b19d1b1797d6c1ce1cb7b0e
Mario Muñoz <mario.munozv@usach.cl>
2022-03-24 09:12:12 -0300

    final

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+$( ".menu" ).on('click touchstart', function(e) {
+    // prevent default anchor click 
+    e.preventDefault();
+    $(".overlay").toggleClass("overlay-open");
+    $("#hamburger-icon").toggleClass("hamburger-open");
+    $(".nav-switch").slideToggle(500);
+});
